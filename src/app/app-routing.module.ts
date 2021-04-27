7d635ee184db5c148a880d9f1f1fce504cceda6d
mjimenezmartin <jimenezmartinmoises@gmail.com>
2021-04-28 00:15:50 +0200

    Add euro-jackpot module as lazy loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'euro-jackpot',
+  },
+  {
+    path: 'euro-jackpot',
+    loadChildren: () => import('./euro-jackpot/euro-jackpot.module').then(mod => mod.EuroJackpotModule),
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
